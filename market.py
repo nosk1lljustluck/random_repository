421094dbdc8beb763fa52b39c111fc5b5e4dffb9
Azizjon Ilkhomov <azizjonilxomov65@gmail.com>
2025-11-06 12:18:41 +0000

    update market.py
    added vegetables class, so now customers can now vegetables as well

--- market.py
+++ market.py
@@ -7,11 +7,13 @@ class supermarket():
         return self.profit
     
     def buy(self):
-        a=input("What you want to buy? eg: frozen foods, fresh fruit, vegetables... ")
-        c=int(input("How much amount do you want to buy? "))
-        buy_new_good=dict[a]()
-        if buy_new_good.can_buy(c):buy_new_good.buy(c)
-        self.profit+=buy_new_good.get_price()*c
+        while True:
+            a=input("What you want to buy? (press 0 to quit): ")
+            if a=="0":break
+            c=int(input("How much amount do you want to buy? "))
+            buy_new_good=dict[a]()
+            if buy_new_good.can_buy(c):buy_new_good.buy(c)
+            self.profit+=buy_new_good.get_price()*c
     
 class fresh_fruit():
     def __init__(self):
@@ -27,9 +29,25 @@ class fresh_fruit():
     def buy(self, amount):
         self.available_amount-=amount
         print("Here is your product, thanks! ")
+        
+class vegetables():
+    def __init__(self):
+        self.price=500
+        self.available_amount=1500
+        
+    def get_price(self):
+        return self.price
+    
+    def can_buy(self, amount):
+        return 1 if self.available_amount>amount else 0
+    
+    def buy(self, amount):
+        self.available_amount-=amount
+        print("Here is your product, thanks! ")
 
 dict={
       "fresh fruit": fresh_fruit,
+      "vegetables": vegetables
       
       }   
     
